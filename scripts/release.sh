fedd6e775b326fbbe4f68f251fe6807ce8d8ffc7
Jack Tzu-Chieh Huang <nightlancer@hotmail.com>
2016-08-16 14:01:02 +0800

    implement auto release build scripts (#54)

---
+++ scripts/release.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+#clone latest branch
+git clone "https://$RELEASE_USER:$RELEASE_TOKEN@github.com/ringcentral/ringcentral-js-integration-commons.git" -b latest release
+npm run release
+
+cd release
+GIT_STATUS=$(git status -s)
+EMPTY_STATUS=' '
+
+if [ $GIT_STATUS -ne $EMPTY_STATUS ]
+  then
+  git add .
+  git commit -m "released at $(date)"
+  git push origin latest
+fi
