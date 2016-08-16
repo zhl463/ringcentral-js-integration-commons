678898d7eb619fcb2c157139039501c33d38c944
Jack Tzu-Chieh Huang <nightlancer@hotmail.com>
2016-08-16 11:14:23 +0800

    prepare release scripts (#52)

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,8 @@ import babel from 'gulp-babel';
 import sourcemaps from 'gulp-sourcemaps';
 import distConfig from './webpack.config';
 import Loganberry from 'loganberry';
+import cp from 'child_process';
+import semver from 'semver';
 
 const TIMEOUT = 30000;
 const argv = yargs.argv;
@@ -123,7 +125,6 @@ gulp.task('test-server', done => {
 });
 
 gulp.task('demo-server', done => {
-  const files = new Set();
   const demoServerPath = path.resolve(__dirname, 'demo-server');
 
   const compiler = webpack(demoServerConfig, err => {
@@ -140,7 +141,25 @@ gulp.task('demo-server', done => {
   });
 });
 
-gulp.task('build', () => (
+async function rm(filepath) {
+  if (await fs.exists(filepath)) {
+    if ((await fs.stat(filepath)).isDirectory()) {
+      await Promise.all(
+        (await fs.readdir(filepath))
+          .map(item => rm(path.resolve(filepath, item)))
+      );
+      await fs.rmdir(filepath);
+    } else {
+      await fs.unlink(filepath);
+    }
+  }
+}
+
+gulp.task('clean', async () => (
+  rm(path.resolve(__dirname, 'build'))
+));
+
+gulp.task('build', ['clean'], () => (
   gulp.src('src/**/*.js')
     .pipe(sourcemaps.init())
     .pipe(babel())
@@ -186,5 +205,129 @@ gulp.task('watch', async () => {
       });
     });
   });
+});
+
+function splitCmd(cmd) {
+  const quotes = [];
+  const quoteReg = /(".*?"|'.*?')/;
+  const replacementReg = /\{\{([0-9]*)\}\}/;
+  let pCmd = cmd;
+  let match = quoteReg.exec(pCmd);
+  while (match) {
+    const replaceStr = `{{${quotes.length}}}`;
+    quotes.push(match[0]);
+    pCmd = pCmd.replace(match[0], replaceStr);
+    match = quoteReg.exec(pCmd);
+  }
+  return pCmd.split(' ').map(token => {
+    const quoteMatch = replacementReg.exec(token);
+    if (quoteMatch && quoteMatch[1]) {
+      const number = parseInt(quoteMatch[1], 10);
+      if (quotes[number]) {
+        return quotes[number];
+      }
+    }
+    return token;
+  });
+}
+
+async function spawn(cmd, opts = {}) {
+  return new Promise((resolve, reject) => {
+    const [program, ...args] = splitCmd(cmd);
+    const options = {
+      stdio: 'inherit',
+    };
+    Object.assign(options, opts);
+    cp.spawn(program, args, options).on('exit', code => {
+      if (code === 0) {
+        resolve();
+      } else {
+        reject(code);
+      }
+    }).on('error', reject);
+  });
+}
+
+gulp.task('release-clean', async () => {
+  if (!await fs.exists('release')) {
+    logger.fatal('Release folder not found');
+    logger.info('Please clone the `latest` branch of your repo into `release` folder');
+    logger.info('by running `git clone ${url to your repo} -b latest release`');
+    throw new Error('Release folder not found');
+  }
+  const files = (await fs.readdir('release')).filter(file => !/^\./.test(file));
+  for (const file of files) {
+    await rm(path.resolve(__dirname, 'release', file));
+  }
+});
+
+gulp.task('release-copy', ['build', 'release-clean'], () => (
+  gulp.src('build/**')
+    .pipe(gulp.dest('release'))
+));
+
+async function bumpVersion(type) {
+  const packageInfo = JSON.parse(await fs.readFile('package.json'));
+  packageInfo.version = semver.inc(packageInfo.version, type);
+
+  const releaseInfo = {};
+  Object.assign(releaseInfo, packageInfo);
+  delete releaseInfo.scripts;
+
+  await fs.writeFile('package.json', JSON.stringify(packageInfo, null, 2), 'utf8');
+  await fs.writeFile('release/package.json', JSON.stringify(releaseInfo, null, 2), 'utf8');
+  return packageInfo.version;
+}
+async function tagSource(version) {
+  await spawn('git add .');
+  await spawn(`git commit -m 'version ${version}'`);
+  await spawn(`git tag -a ${version} -m 'version ${version}'`);
+}
+async function tagRelease(version) {
+  const opts = {
+    cwd: path.resolve('release'),
+  };
+  await spawn('git add .', opts);
+  await spawn(`git commit -m 'version ${version}'`, opts);
+}
+async function checkForChanges(gitPath = __dirname) {
+  const res = cp.execSync('git status -s', {
+    cwd: path.resolve(gitPath),
+  })
+    .toString()
+    .split(/\r?\n/)
+    .map(line => line.trim())
+    .filter(line => (
+      line !== ''
+  ));
+  return res.length > 0;
+}
+
+gulp.task('release-package-patch', ['release-copy'], async () => {
+  if (await checkForChanges(path.resolve('release'))) {
+    const version = await bumpVersion('patch');
+    await tagSource(version);
+    await tagRelease(version);
+  } else {
+    logger.info('No changes found in build...');
+  }
+});
+gulp.task('release-package-minor', ['release-copy'], async () => {
+  if (await checkForChanges(path.resolve('release'))) {
+    const version = await bumpVersion('minor');
+    await tagSource(version);
+    await tagRelease(version);
+  } else {
+    logger.info('No changes found in build...');
+  }
+});
+gulp.task('release-package-major', ['release-copy'], async () => {
+  if (await checkForChanges(path.resolve('release'))) {
+    const version = await bumpVersion('major');
+    await tagSource(version);
+    await tagRelease(version);
+  } else {
+    logger.info('No changes found in build...');
+  }
 });
 
