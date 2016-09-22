c4b6d3e60d4ece1bd322761e59a8fbec73c54426
Jack Tzu-Chieh Huang <nightlancer@hotmail.com>
2016-09-22 14:43:53 +0800

    clean up the repo for some restructuring for better collaboration (#62)

--- README.md
+++ README.md
@@ -4,120 +4,26 @@
 [![Coverage Status](https://coveralls.io/repos/github/ringcentral/ringcentral-js-integration-commons/badge.svg?branch=master)](https://coveralls.io/github/ringcentral/ringcentral-js-integration-commons?branch=master)
 
 Integration common javascript core library.
-In this project we aim to:
+In this project we aim to provide common modules for building integration apps that are:
+- Feature specific
+- Composible
 
-1. Refactor and converge all integration js code bases into a common package.
-2. Utilize official ringcentral js sdk libraries as much as possible.
-
-# Project Init
-
-- [x] Server-side test framework setup
-- [x] Browser-side test framework setup
-- [x] Coding style guide document
-- [x] Eslint check setup
-- [x] Webpack setup for distributing code
-- [x] CI setup
-
-#To start
-
-1. Install dependencies
-
-    ```bash
-    npm install
-    ```
-2. Create accounts.json
-
-    ```json
-    {
-      "app": {
-        "appKey": "${your app key}",
-        "appSecret": "${your app secret}"
-      },
-      "apiServer": "${apiServer address}",
-      "brand": {
-        "name": "${brand name}",
-        "id": "${brand id}"
-      },
-      "user": {
-        "username": "${RingCentral sandbox account username}",
-        "extension": "${RingCentral sandbox account extension}",
-        "password": "${RingCentral sandbox account password}"
-      }
-    }
-    ```
-
-#Coding Style
-
-Please refer to [airbnb/javascript](https://github.com/airbnb/javascript).
-We will use the recommended linter settings and jscs settings, and adjust as we progress.
-
-Also refer to .eslintrc for our own overrides.
-
-#Linting
-
-Run linter from commandline:
+Modules
 ---
 
-```bash
-npm run eslint
-```
-Linter in Editors:
+- [x] Auth
+- [x] Subscription
+- [x] Storage
+- [ ] DialingPlan
+- [ ] ExtensionInfo
+- [ ] AccountInfo
+- [ ] PhoneNumbers
+- more...
+
+Contribution
 ---
 
-```bash
-npm install -g eslint babel-eslint eslint-config-airbnb eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react
-```
+Please fork the project and read the following:
 
-1. SublimeText
-
-    Install sublime package control from [packagecontrol.io](https://packagecontrol.io/installation)
-
-    Required Packages:
-        * SublimeLinter
-        * SublimeLinter-contrib-eslint
-        * EditorConfig
-    Recommended Pakcages:
-        * Babel
-        * GitGutter
-
-    Linting should happen on file write.
-
-2. VSCode
-
-    Required Extensions:
-        * ESLint
-        * EditorConfig for VS Code
-    Recommended Extensions:
-        * Babel ES6/ES7
-
-    The linting errors and warnings should show as ~~~ under the code.
-
-
-#Tests
-
-Run all tests:
-```bash
-npm run test
-```
-
-Run tests in specific test folder or file:
-```bash
-npm run quick-test -- --folder folder1 --folder folder2,folder3
-npm run quick-test -- --folder test/lib
-
-npm run quick-test -- --file test/lib/add-module.test.js
-```
-Both multiple --folder/--file declaration, or comma-delimmated list will work too.
-
-#Test in Browsers
-
-```bash
-npm run test-Browser
-```
-
-Open localhost:8190 in your browser to run the tests.
-
-#Contribution
-
-Please fork and contribute via PR.
+- [Contribution Guide](docs/contribute.md)
 
--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,8 @@ import path from 'path';
 import fs from 'fs-promise';
 import webpack from 'webpack';
 import WebpackDevServer from 'webpack-dev-server';
-import testServerConfig from './test-server/webpack.config';
-import demoServerConfig from './demo-server/webpack.config';
 import babel from 'gulp-babel';
 import sourcemaps from 'gulp-sourcemaps';
-import distConfig from './webpack.config';
 import Loganberry from 'loganberry';
 import cp from 'child_process';
 import semver from 'semver';
@@ -93,55 +90,38 @@ function ensurePosixPath(str) {
   return str.split(path.sep).join('/');
 }
 
-gulp.task('test-server', done => {
-  const files = new Set();
-  const testServerPath = path.resolve(__dirname, 'test-server');
-  gulp.src(getTestSources())
-    .pipe(through.obj((file, enc, cb) => {
-      files.add(ensurePosixPath(path.relative(testServerPath, file.path).replace(jsExt, '')));
-      cb();
-    }))
-    .on('finish', async () => {
-      const loaderScript = `mocha.setup('bdd');
-        mocha.timeout(${TIMEOUT});
-        ${[...files].map(f => `require('${f}');`).join('\n')}
-        mocha.run();
-      `;
+// gulp.task('test-server', done => {
+//   const files = new Set();
+//   const testServerPath = path.resolve(__dirname, 'test-server');
+//   gulp.src(getTestSources())
+//     .pipe(through.obj((file, enc, cb) => {
+//       files.add(ensurePosixPath(path.relative(testServerPath, file.path).replace(jsExt, '')));
+//       cb();
+//     }))
+//     .on('finish', async () => {
+//       const loaderScript = `mocha.setup('bdd');
+//         mocha.timeout(${TIMEOUT});
+//         ${[...files].map(f => `require('${f}');`).join('\n')}
+//         mocha.run();
+//       `;
 
-      await fs.writeFile(path.resolve(__dirname, './test-server/auto-loader.js'), loaderScript);
+//       await fs.writeFile(path.resolve(__dirname, './test-server/auto-loader.js'), loaderScript);
 
-      await new Promise((resolve, reject) => {
-        const compiler = webpack(testServerConfig, err => {
-          if (err) return reject(err);
+//       await new Promise((resolve, reject) => {
+//         const compiler = webpack(testServerConfig, err => {
+//           if (err) return reject(err);
 
-          new WebpackDevServer(compiler, {
-            contentBase: testServerPath,
-            publicPath: testServerConfig.output.publicPath,
-            hot: true,
-          }).listen(8190);
-          return resolve();
-        });
-      });
-      done();
-    });
-});
-
-gulp.task('demo-server', done => {
-  const demoServerPath = path.resolve(__dirname, 'demo-server');
-
-  const compiler = webpack(demoServerConfig, err => {
-    if (err) done(err);
-    new WebpackDevServer(compiler, {
-      contentBase: demoServerPath,
-      publicPath: demoServerConfig.output.publicPath,
-      hot: true,
-    }).listen(8191, () => {
-      setTimeout(() => {
-        logger.info('listening to port 8191...');
-      }, 5000); // not exactly sure when the server is available...
-    });
-  });
-});
+//           new WebpackDevServer(compiler, {
+//             contentBase: testServerPath,
+//             publicPath: testServerConfig.output.publicPath,
+//             hot: true,
+//           }).listen(8190);
+//           return resolve();
+//         });
+//       });
+//       done();
+//     });
+// });
 
 async function rm(filepath) {
   if (await fs.exists(filepath)) {
@@ -168,46 +148,6 @@ gulp.task('build', ['clean'], () => (
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('build'))
 ));
-
-gulp.task('dist', async () => {
-  await new Promise((resolve, reject) => {
-    webpack(distConfig, err => {
-      if (err) reject(err);
-      resolve();
-    });
-  });
-  await new Promise((resolve, reject) => {
-    distConfig.plugins = [
-      new webpack.optimize.UglifyJsPlugin({
-        compressor: {
-          warnings: false,
-        },
-        output: {
-          comments: false,
-        },
-      }),
-    ];
-    distConfig.output.filename = 'ringcentral-js-integration-commons.min.js';
-    webpack(distConfig, err => {
-      if (err) reject(err);
-      resolve();
-    });
-  });
-});
-
-gulp.task('watch', async () => {
-  await new Promise((resolve, reject) => {
-    distConfig.watch = true;
-    const compiler = webpack(distConfig);
-
-    compiler.watch({}, err => {
-      if (err) reject(err);
-      compiler.run(compileErr => {
-        if (compileErr) reject(compileErr);
-      });
-    });
-  });
-});
 
 function splitCmd(cmd) {
   const quotes = [];
@@ -271,71 +211,3 @@ gulp.task('release', ['release-copy'], async () => {
   packageInfo.main = 'rc-phone.js';
   await fs.writeFile('release/package.json', JSON.stringify(packageInfo, null, 2));
 });
-
-
-
-// async function bumpVersion(type) {
-//   const packageInfo = JSON.parse(await fs.readFile('package.json'));
-//   packageInfo.version = semver.inc(packageInfo.version, type);
-
-//   const releaseInfo = {};
-//   Object.assign(releaseInfo, packageInfo);
-//   delete releaseInfo.scripts;
-
-//   await fs.writeFile('package.json', JSON.stringify(packageInfo, null, 2), 'utf8');
-//   await fs.writeFile('release/package.json', JSON.stringify(releaseInfo, null, 2), 'utf8');
-//   return packageInfo.version;
-// }
-// async function tagSource(version) {
-//   await spawn('git add .');
-//   await spawn(`git commit -m 'version ${version}'`);
-//   await spawn(`git tag -a ${version} -m 'version ${version}'`);
-// }
-// async function tagRelease(version) {
-//   const opts = {
-//     cwd: path.resolve('release'),
-//   };
-//   await spawn('git add .', opts);
-//   await spawn(`git commit -m 'version ${version}'`, opts);
-// }
-// async function checkForChanges(gitPath = __dirname) {
-//   const res = cp.execSync('git status -s', {
-//     cwd: path.resolve(gitPath),
-//   })
-//     .toString()
-//     .split(/\r?\n/)
-//     .map(line => line.trim())
-//     .filter(line => (
-//       line !== ''
-//   ));
-//   return res.length > 0;
-// }
-
-// gulp.task('release-package-patch', ['release-copy'], async () => {
-//   if (await checkForChanges(path.resolve('release'))) {
-//     const version = await bumpVersion('patch');
-//     await tagSource(version);
-//     await tagRelease(version);
-//   } else {
-//     logger.info('No changes found in build...');
-//   }
-// });
-// gulp.task('release-package-minor', ['release-copy'], async () => {
-//   if (await checkForChanges(path.resolve('release'))) {
-//     const version = await bumpVersion('minor');
-//     await tagSource(version);
-//     await tagRelease(version);
-//   } else {
-//     logger.info('No changes found in build...');
-//   }
-// });
-// gulp.task('release-package-major', ['release-copy'], async () => {
-//   if (await checkForChanges(path.resolve('release'))) {
-//     const version = await bumpVersion('major');
-//     await tagSource(version);
-//     await tagRelease(version);
-//   } else {
-//     logger.info('No changes found in build...');
-//   }
-// });
-
