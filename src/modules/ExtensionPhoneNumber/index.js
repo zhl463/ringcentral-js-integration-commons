0d474b07100f1652060ed492ec46fe0686697a4d
Jack Tzu-Chieh Huang <nightlancer@hotmail.com>
2016-10-26 15:17:45 +0800

    fix bug where ExtensionPhoneNumber return undefined for phoneNumbers (#80)

--- src/modules/ExtensionPhoneNumber/index.js
+++ src/modules/ExtensionPhoneNumber/index.js
@@ -66,7 +66,7 @@ export default class ExtensionPhoneNumber extends RcModule {
   }
 
   get phoneNumbers() {
-    return this.data.phoneNumbers;
+    return this.data.extensionPhoneNumbers;
   }
 
   get companyNumbers() {
