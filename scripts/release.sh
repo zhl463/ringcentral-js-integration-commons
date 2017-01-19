9f42bc8d6661621945ba52f037f6cf4d829e07c9
Jack Tzu-Chieh Huang <nightlancer@hotmail.com>
2017-01-19 13:20:42 +0800

    fix release.sh (#131)

--- scripts/release.sh
+++ scripts/release.sh
@@ -10,7 +10,7 @@ if [[ $(git status -s) != '' ]]
   then
   git config user.email "integrations@ringcentral.com" > /dev/null
   git config user.name "RingCentral Integrations Team" > /dev/null
-  git add --all . /dev/null
-  git commit -m "released at $(date)" /dev/null
-  git push origin latest -f /dev/null
+  git add --all . > /dev/null
+  git commit -m "released at $(date)" > /dev/null
+  git push origin latest -f > /dev/null
 fi
