7329a360fe60412f65c8adf34503e33b295a78d3
Jack Tzu-Chieh Huang <nightlancer@hotmail.com>
2017-08-25 20:25:57 -0500

    put popWindow function into separate file and expose it (#382)

--- src/lib/ProxyFrameController.js
+++ src/lib/ProxyFrameController.js
@@ -1,30 +1,4 @@
-
-function popWindow(url, id, w, h) {
-  // Fixes dual-screen position                         Most browsers      Firefox
-  const dualScreenLeft = window.screenLeft !== undefined ? window.screenLeft : screen.left;
-  const dualScreenTop = window.screenTop !== undefined ? window.screenTop : screen.top;
-
-  const width = screen.width || window.outerWidth;
-  const height = screen.height || window.innerHeight;
-
-  const left = ((width / 2) - (w / 2)) + dualScreenLeft;
-  const top = ((height / 2) - (h / 2)) + dualScreenTop;
-  const newWindow = window.open(
-    url,
-    id,
-    `scrollbars=yes, width=${w}, height=${h}, top=${top}, left=${left}`,
-  );
-
-  // Puts focus on the newWindow
-  if (window.focus) {
-    try {
-      newWindow.focus();
-    } catch (e) {
-      /* falls through */
-    }
-  }
-  return newWindow;
-}
+import popWindow from './popWindow';
 
 export default class ProxyFrameController {
   constructor({
