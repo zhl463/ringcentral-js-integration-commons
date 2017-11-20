1bc05abd9ba44b86864b421e06b672316fd2cb8f
Embbnux Ji <embbnux@embbnux.com>
2017-11-20 10:43:40 +0800

    remove uri from acccount phone number (#464)

--- src/modules/AccountPhoneNumber/getReducer.js
+++ src/modules/AccountPhoneNumber/getReducer.js
@@ -1,8 +1,13 @@
+import removeUri from '../../lib/removeUri';
+
 export function getDataReducer(types) {
   return (state = null, { type, data }) => {
     switch (type) {
       case types.fetchSuccess:
-        return data;
+        return data && data.map(item => ({
+          ...item,
+          extension: removeUri(item.extension),
+        }));
       case types.resetSuccess:
         return null;
       default:
