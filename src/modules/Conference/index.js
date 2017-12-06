8c522516a4388a2498f9b40cacd913f774097540
Kevin Zeng <zengfenfei@gmail.com>
2017-12-06 10:44:44 +0800

    Conference for google V4 (#489)
    * Conference for Google V4
    
    * Conference additional dial in numbers
    
    * Conference update methods proxify
    
    * Conference allow join before host

--- src/modules/Conference/index.js
+++ src/modules/Conference/index.js
@@ -1,15 +1,20 @@
 import mask from 'json-mask';
 import { Module } from '../../lib/di';
 import DataFetcher from '../../lib/DataFetcher';
+import createSimpleReducer from '../../lib/createSimpleReducer';
+import actionTypes from './actionTypes';
+import proxify from '../../lib/proxy/proxify';
 
-const DEFAULT_MASK = 'phoneNumber,hostCode,participantCode,phoneNumbers(country,phoneNumber)';
+const DEFAULT_MASK = 'phoneNumber,hostCode,participantCode,phoneNumbers(country,phoneNumber),allowJoinBeforeHost';
 
 /**
  * @class
  * @description Conference managing module
  */
 @Module({
-  deps: ['Client', 'RegionSettings', { dep: 'ConferenceOptions', optional: true }]
+  deps: [
+    'Client', 'Storage', 'RegionSettings', { dep: 'ConferenceOptions', optional: true }
+  ]
 })
 export default class Conference extends DataFetcher {
   /**
@@ -21,6 +26,7 @@ export default class Conference extends DataFetcher {
   constructor({
     client,
     regionSettings,
+    storage,
     ...options
   }) {
     super({
@@ -30,31 +36,48 @@ export default class Conference extends DataFetcher {
         await client.account().extension().conferencing().get(),
         DEFAULT_MASK,
       ),
+      actionTypes,
+      storage,
       ...options,
     });
-    this.addSelector(
-      'conferenceNumbers',
-      () => regionSettings.countryCode,
-      () => this.data,
-      (isoCode, data) => {
-        if (!data) {
-          return data;
-        }
-        const countrys = data.phoneNumbers.find(value => value.country.isoCode === isoCode);
-        if (typeof countrys === 'undefined') {
-          return data;
-        }
-        return {
-          ...data,
-          phoneNumber: countrys.phoneNumber,
-          phoneNumbers: data.phoneNumbers.filter(value =>
-            value.phoneNumber !== countrys.phoneNumber),
-        };
-      }
-    );
+    this._dialInNumberStorageKey = 'conferenceDialInNumber';
+    this._additionalNumbersStorageKey = 'conferenceAdditionalNumbers';
+    this._storage.registerReducer({
+      key: this._dialInNumberStorageKey,
+      reducer: createSimpleReducer(this.actionTypes.updateDialInNumber, 'dialInNumber'),
+    });
+    this._storage.registerReducer({
+      key: this._additionalNumbersStorageKey,
+      reducer: createSimpleReducer(this.actionTypes.updateAdditionalNumbers, 'additionalNumbers'),
+    });
   }
 
-  get conferenceNumbers() {
-    return this._selectors.conferenceNumbers();
+  @proxify
+  async updateEnableJoinBeforeHost(allowJoinBeforeHost) {
+    const data = await this._client.account().extension().conferencing()
+      .put({ allowJoinBeforeHost });
+    this._store.dispatch({ type: this.actionTypes.fetchSuccess, data });
+  }
+
+  @proxify
+  updateDialInNumber(dialInNumber) {
+    this._store.dispatch({ type: this.actionTypes.updateDialInNumber, dialInNumber });
+  }
+
+  @proxify
+  updateAdditionalNumbers(additionalNumbers) {
+    this._store.dispatch({ type: this.actionTypes.updateAdditionalNumbers, additionalNumbers });
+  }
+
+  get additionalNumbers() {
+    return this._storage.getItem(this._additionalNumbersStorageKey) || [];
+  }
+
+  get dialInNumber() {
+    return this._storage.getItem(this._dialInNumberStorageKey) || this.data.phoneNumber;
+  }
+
+  _shouldFetch() {
+    return !this._tabManager || this._tabManager.active;
   }
 }
